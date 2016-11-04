91a534c2c5f5241cf6dbdce68ed77427b86175fe
sajarufe <sajarufe@uc.cl>
2016-11-03 21:33:41 -0300

    auth fix

--- routes/users.js
+++ routes/users.js
@@ -16,101 +16,101 @@ const User = require('../models/user');
 const dashboard = 'http://localhost:3000/'
 
 function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		res.redirect(dashboard);
-	} else {
-		return next();
-	}
+  if(req.isAuthenticated()){
+    res.redirect(dashboard);
+  } else {
+    return next();
+  }
 }
 
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('login')
+  res.render('login')
 });
 
 // Register
 router.get('/register', function(req, res){
-	res.render('register');
+  res.render('register');
 });
 
 // Login
 router.get('/login',ensureAuthenticated, function(req, res){
-	res.render('login');
+  res.render('login');
 });
 
 // Register User
 router.post('/register', function(req, res){
-	var name = req.body.name;
-	var email = req.body.email;
-	var username = req.body.username;
-	var password = req.body.password;
-	var password2 = req.body.password2;
-	var address = req.body.address;
-	var bloodtype = req.body.bloodtype;
-	var birthday = req.body.birthday;
-	var cardnumber = req.body.cardnumber;
-	var cvs = req.body.cvs;
-	var accounttype = req.body.accounttype;
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  var address = req.body.address;
+  var bloodtype = req.body.bloodtype;
+  var birthday = req.body.birthday;
+  var cardnumber = req.body.cardnumber;
+  var cvs = req.body.cvs;
+  var accounttype = req.body.accounttype;
 
-	// Validation
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('username', 'Username is required').notEmpty();
-	req.checkBody('address', 'Address is required').notEmpty();
-	req.checkBody('bloodtype', 'Blood type is required').notEmpty();
-	req.checkBody('accounttype', 'Account type is required').notEmpty();
-	req.checkBody('cardnumber', 'Card Number is required and should have 16 digits').notEmpty().len(16);
-	req.checkBody('cvs', 'Card Verification Number is required and should have 3 digits').notEmpty().len(3);
-	req.checkBody('password', 'Password is required').notEmpty();
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('address', 'Address is required').notEmpty();
+  req.checkBody('bloodtype', 'Blood type is required').notEmpty();
+  req.checkBody('accounttype', 'Account type is required').notEmpty();
+  req.checkBody('cardnumber', 'Card Number is required and should have 16 digits').notEmpty().len(16);
+  req.checkBody('cvs', 'Card Verification Number is required and should have 3 digits').notEmpty().len(3);
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-	var errors = req.validationErrors();
+  var errors = req.validationErrors();
 
-	if(errors){
-		res.render('register',{
-			errors:errors
-		});
-	} else {
-		var newUser = new User({
-			name: name,
-			email:email,
-			address: address,
-			bloodtype: bloodtype,
-			accounttype: accounttype,
-			cardnumber: cardnumber,
-			cvs: cvs,
-			username: username,
-			password: password
+  if(errors){
+    res.render('register',{
+      errors:errors
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email:email,
+      address: address,
+      bloodtype: bloodtype,
+      accounttype: accounttype,
+      cardnumber: cardnumber,
+      cvs: cvs,
+      username: username,
+      password: password
 
-		});
+    });
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
-			console.log(user);
-		});
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
 
-		req.flash('success_msg', 'You are registered and can now login');
+    req.flash('success_msg', 'You are registered and can now login');
 
-		res.redirect('./login');
-	}
+    res.redirect('./login');
+  }
 });
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
    User.getUserByUsername(username, function(err, user){
-   	if(err) throw err;
-   	if(!user){
-   		return done(null, false, {message: 'Unknown User'});
-   	}
+    if(err) throw err;
+    if(!user){
+      return done(null, false, {message: 'Unknown User'});
+    }
 
-   	User.comparePassword(password, user.password, function(err, isMatch){
-   		if(err) throw err;
-   		if(isMatch){
-   			return done(null, user);
-   		} else {
-   			return done(null, false, {message: 'Invalid password'});
-   		}
-   	});
+    User.comparePassword(password, user.password, function(err, isMatch){
+      if(err) throw err;
+      if(isMatch){
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Invalid password'});
+      }
+    });
    });
   }));
 
@@ -127,61 +127,78 @@ passport.deserializeUser(function(id, done) {
 router.post('/login',
   passport.authenticate('local'),
   function(req, res) {
-  	const token = assign_token(req.user);
-    res.redirect('./chat/chat_room/2/' + token);
+    const token = assign_token(req.user);
+    res.redirect('http://localhost:3000/chat/chat_room/2/' + token);
   });
 
 router.get('/logout', function(req, res){
-	req.logout();
+  req.logout();
 
-	req.flash('success_msg', 'You are logged out');
+  req.flash('success_msg', 'You are logged out');
 
-	res.redirect('./login');
+  res.redirect('./login');
 });
 
 router.get('/auth', authenticate_token);
 
 function assign_token(user){
-	var token = randtoken.generate(32);	
-	client.exists(user.id, function(err, reply){
-		if(reply==1){
-			console.log(client.get(user.id));
-			client.del(user.id);
-			var to_remove = client.zscan("tokens_ttl", 0, "MATCH", `${user.id},${user.username}*`)[1][0];
-			var token = to_remove.replace(`${user.id},${user.username}`, '')
-			client.zrem(to_remove);
-			client.srem(token);
-			client.del(token);
-		}
-		client.zadd(["tokens_ttl", moment(Date.now()).utc().add(2, 'hours').unix(), `${user.id},${user.username},${token}`], function(err, response){
+  var token = randtoken.generate(32); 
+  client.exists(user.id, function(err, reply){
+    if(reply==1){
+      var to_remove;
+      client.del(user.id);
+      client.zscan(["tokens_ttl", 0, "MATCH", `${user.id},${user.username}*`], function(err, response){
+        if(!err){
+          to_remove = response[1][0];          
+          var token = to_remove.replace(`${user.id},${user.username}`, '')
+          client.zrem(to_remove, function(err, response){});
+          client.srem(token, function(err, response){});
+          client.del(token);
+        }
+      });
+    }   
+  });
+  client.zadd(["tokens_ttl", moment(Date.now()).utc().add(2, 'hours').unix(), `${user.id},${user.username},${token}`], function(err, response){
+    
+  });
+  client.sadd(["tokens", token], function(err, response){
 
-		});
-		client.sadd(["tokens", token], function(err, response){
-
-		});
-		client.set([token, user.id]);
-		client.set([user.id, token]);
-	});
+  });
+  client.set([token, user.id]);
+  client.set([user.id, token]);
   return token;
 }
 
 function authenticate_token(req, res){
-	var token = req.get("token");
-	client.sismember("tokens", token, function(err, reply){
-		if(reply==1){
-			var user_id = client.get(token)
-			body = {
-				user_id: user_id,
-				username: getUserById(user_id).username
-			}
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'application/json');
-			res.json(body);
-		}
-		else{
-			res.statusCode = 401;
-		}
-	});
+  var token = req.get("token");
+  client.sismember(["tokens", token], function(err, reply){
+    if(reply==1){
+      var user_id;
+      var username;
+      client.get(token, function(err, response){
+        user_id = response;
+        User.getUserById(user_id, function(err, user){
+          if(!err){
+            username = user.username;
+            body = {
+              user_id: user_id,
+              username: username
+            }
+            console.log(body);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(body);
+          }
+          else{
+            console.log(err);
+          }
+        }); 
+      });
+    }
+    else{      
+      res.statusCode = 401;
+    }
+  });
 }
 
 module.exports = router;
